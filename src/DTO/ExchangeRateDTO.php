581616bca7f54d058327d37320983b0c1fe375c5
oleg <tokyospliff3@gmail.com>
2025-03-10 00:26:41 +0200

    create dto

---
+++ src/DTO/ExchangeRateDTO.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\DTO;
+
+use App\Exception\ExchangeRateException;
+use DateTimeInterface;
+
+class ExchangeRateDTO
+{
+    private string $currency_pair;
+    private array $rates;
+    private DateTimeInterface $timestamp;
+
+    /**
+     * @throws ExchangeRateException
+     */
+    public function __construct(string $currency_pair, array $rates, DateTimeInterface $timestamp)
+    {
+        $this->validateRates($rates);
+        $this->currency_pair = $currency_pair;
+        $this->rates = $rates;
+        $this->timestamp = $timestamp;
+    }
+
+    /**
+     * @throws ExchangeRateException
+     */
+    private function validateRates(array $rates): void
+    {
+        foreach ($rates as $currency => $rate) {
+            if (!is_numeric($rate) || $rate < 0) {
+                throw ExchangeRateException::fromValidationError(
+                    sprintf('Invalid rate value for currency %s: %s', $currency, $rate)
+                );
+            }
+        }
+    }
+
+    public function getCurrencyPair(): string
+    {
+        return $this->currency_pair;
+    }
+
+    public function getRates(): array
+    {
+        return $this->rates;
+    }
+
+    public function getTimestamp(): DateTimeInterface
+    {
+        return $this->timestamp;
+    }
+
+    /**
+     * @throws ExchangeRateException
+     */
+    public function getRate(string $currency): float
+    {
+        if (!isset($this->rates[$currency])) {
+            throw ExchangeRateException::fromValidationError(
+                sprintf('Rate for currency %s not found', $currency)
+            );
+        }
+        return $this->rates[$currency];
+    }
+}
