a18550f671fe166c5db61b3cdc66f2119e549c7c
oleg <tokyospliff3@gmail.com>
2025-03-10 00:26:33 +0200

    create command

--- src/Command/UpdateExchangeRatesCommand.php
+++ src/Command/UpdateExchangeRatesCommand.php
@@ -2,15 +2,17 @@
 
 namespace App\Command;
 
+use App\Exception\ExchangeRateException;
 use App\Service\ExchangeRateService;
-use Symfony\Component\Console\Attribute\AsCommand;
+use Exception;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
 
-#[AsCommand(name: 'app:update-exchange-rates')]
 class UpdateExchangeRatesCommand extends Command
 {
+    protected static $defaultName = 'app:update-exchange-rates';
     private ExchangeRateService $exchangeRateService;
 
     public function __construct(ExchangeRateService $exchangeRateService)
@@ -19,10 +21,27 @@ class UpdateExchangeRatesCommand extends Command
         $this->exchangeRateService = $exchangeRateService;
     }
 
+    protected function configure(): void
+    {
+        $this
+            ->setDescription('Updates the exchange rates from the external API')
+            ->setHelp('This command allows you to update the exchange rates from the external API and save them to the database');
+    }
+
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $this->exchangeRateService->updateExchangeRates();
-        $output->writeln('Exchange rates updated successfully.');
-        return Command::SUCCESS;
+        $io = new SymfonyStyle($input, $output);
+
+        try {
+            $this->exchangeRateService->updateExchangeRates();
+            $io->success('Exchange rates updated successfully.');
+            return Command::SUCCESS;
+        } catch (ExchangeRateException $e) {
+            $io->error('Failed to update exchange rates: ' . $e->getMessage());
+            return Command::FAILURE;
+        } catch (Exception $e) {
+            $io->error('An unexpected error occurred: ' . $e->getMessage());
+            return Command::FAILURE;
+        }
     }
 }
