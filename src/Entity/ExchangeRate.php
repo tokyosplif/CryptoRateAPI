f4c1364a0bc7206e430a5342b0972a3508035c2c
oleg <tokyospliff3@gmail.com>
2025-03-10 00:27:34 +0200

    update entity

--- src/Entity/ExchangeRate.php
+++ src/Entity/ExchangeRate.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use DateTimeInterface;
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\DBAL\Types\Types;
 
 #[ORM\Entity]
 class ExchangeRate
@@ -12,27 +13,15 @@ class ExchangeRate
     #[ORM\GeneratedValue]
     #[ORM\Column(type: "integer")]
     private int $id;
+
     #[ORM\Column(type: "string")]
-    private string $currencyPair;
-    #[ORM\Column(type: "decimal", precision: 15, scale: 8)]
-    private float $rateUSD;
-    #[ORM\Column(type: "decimal", precision: 15, scale: 8)]
-    private float $rateEUR;
-    #[ORM\Column(type: "decimal", precision: 15, scale: 8)]
-    private float $rateGBP;
+    private string $currency_pair;
+
+    #[ORM\Column(type: Types::JSON)]
+    private array $rates = [];
+
     #[ORM\Column(type: "datetime")]
     private DateTimeInterface $timestamp;
-
-    public function getRateGBP(): float
-    {
-        return $this->rateGBP;
-    }
-
-    public function setRateGBP(float $rateGBP): self
-    {
-        $this->rateGBP = $rateGBP;
-        return $this;
-    }
 
     public function getId(): int
     {
@@ -41,34 +30,23 @@ class ExchangeRate
 
     public function getCurrencyPair(): string
     {
-        return $this->currencyPair;
+        return $this->currency_pair;
     }
 
     public function setCurrencyPair(string $currencyPair): self
     {
-        $this->currencyPair = $currencyPair;
+        $this->currency_pair = $currencyPair;
         return $this;
     }
 
-    public function getRateUSD(): float
+    public function getRates(): array
     {
-        return $this->rateUSD;
+        return $this->rates;
     }
 
-    public function setRateUSD(float $rateUSD): self
+    public function setRates(array $rates): self
     {
-        $this->rateUSD = $rateUSD;
-        return $this;
-    }
-
-    public function getRateEUR(): float
-    {
-        return $this->rateEUR;
-    }
-
-    public function setRateEUR(float $rateEUR): self
-    {
-        $this->rateEUR = $rateEUR;
+        $this->rates = $rates;
         return $this;
     }
 
@@ -83,3 +61,4 @@ class ExchangeRate
         return $this;
     }
 }
+
