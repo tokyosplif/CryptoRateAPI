8e6be0f99f76802b4ed18a7ffd77be91c3da1bb9
oleg <tokyospliff3@gmail.com>
2025-03-10 23:08:53 +0200

    update exception

--- src/Exception/ExchangeRateException.php
+++ src/Exception/ExchangeRateException.php
@@ -4,41 +4,27 @@ namespace App\Exception;
 
 use Exception;
 use Throwable;
-use Psr\Log\LoggerInterface;
 
 class ExchangeRateException extends Exception
 {
-    private LoggerInterface $logger;
-
-    public function __construct(string $message, LoggerInterface $logger, int $code = 0, ?Throwable $previous = null)
+    public function __construct(string $message, int $code = 0, ?Throwable $previous = null)
     {
         parent::__construct($message, $code, $previous);
-        $this->logger = $logger;
-        $this->logError();
     }
 
-    private function logError(): void
-    {
-        $this->logger->error('ExchangeRateException: ' . $this->getMessage(), [
-            'stack_trace' => $this->getTraceAsString()
-        ]);
-    }
-
-    public static function fromApiError(string $message, LoggerInterface $logger, ?Throwable $previous = null): self
+    public static function fromApiError(string $message, ?Throwable $previous = null): self
     {
         return new self(
             sprintf('Exchange rate API error: %s', $message),
-            $logger,
             $previous?->getCode() ?? 0,
             $previous
         );
     }
 
-    public static function fromValidationError(string $message, LoggerInterface $logger, ?Throwable $previous = null): self
+    public static function fromValidationError(string $message, ?Throwable $previous = null): self
     {
         return new self(
             sprintf('Validation error: %s', $message),
-            $logger,
             $previous?->getCode() ?? 0,
             $previous
         );
