b0002b51ea0f3741c7378c870bd2cf64c34494f5
oleg <tokyospliff3@gmail.com>
2025-03-10 21:57:16 +0200

    update exception

--- src/Exception/ExchangeRateException.php
+++ src/Exception/ExchangeRateException.php
@@ -4,22 +4,41 @@ namespace App\Exception;
 
 use Exception;
 use Throwable;
+use Psr\Log\LoggerInterface;
 
 class ExchangeRateException extends Exception
 {
-    public static function fromApiError(string $message, ?Throwable $previous = null): self
+    private LoggerInterface $logger;
+
+    public function __construct(string $message, LoggerInterface $logger, int $code = 0, ?Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+        $this->logger = $logger;
+        $this->logError();
+    }
+
+    private function logError(): void
+    {
+        $this->logger->error('ExchangeRateException: ' . $this->getMessage(), [
+            'stack_trace' => $this->getTraceAsString()
+        ]);
+    }
+
+    public static function fromApiError(string $message, LoggerInterface $logger, ?Throwable $previous = null): self
     {
         return new self(
             sprintf('Exchange rate API error: %s', $message),
+            $logger,
             $previous?->getCode() ?? 0,
             $previous
         );
     }
 
-    public static function fromValidationError(string $message, ?Throwable $previous = null): self
+    public static function fromValidationError(string $message, LoggerInterface $logger, ?Throwable $previous = null): self
     {
         return new self(
             sprintf('Validation error: %s', $message),
+            $logger,
             $previous?->getCode() ?? 0,
             $previous
         );
@@ -27,6 +46,6 @@ class ExchangeRateException extends Exception
 
     public function getFormattedMessage(): string
     {
-        return $this->getMessage() . "\n" . $this->getTraceAsString();
+        return $this->getMessage();
     }
 }
