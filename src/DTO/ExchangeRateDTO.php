1ab010fc5eea35b958f99eaae36401711125c9e8
oleg <tokyospliff3@gmail.com>
2025-03-11 00:31:37 +0200

    update dto

--- src/DTO/ExchangeRateDTO.php
+++ src/DTO/ExchangeRateDTO.php
@@ -2,7 +2,6 @@
 
 namespace App\DTO;
 
-use App\Exception\ExchangeRateException;
 use DateTimeInterface;
 
 class ExchangeRateDTO
@@ -11,12 +10,8 @@ class ExchangeRateDTO
     private array $rates;
     private DateTimeInterface $timestamp;
 
-    /**
-     * @throws ExchangeRateException
-     */
     public function __construct(string $currency_pair, array $rates, DateTimeInterface $timestamp)
     {
-        $this->validateRates($rates);
         $this->currency_pair = $currency_pair;
         $this->rates = $rates;
         $this->timestamp = $timestamp;
@@ -35,32 +30,5 @@ class ExchangeRateDTO
     public function getTimestamp(): DateTimeInterface
     {
         return $this->timestamp;
-    }
-
-    /**
-     * @throws ExchangeRateException
-     */
-    private function validateRates(array $rates): void
-    {
-        foreach ($rates as $currency => $rate) {
-            if (!is_numeric($rate) || $rate < 0) {
-                throw ExchangeRateException::fromValidationError(
-                    sprintf('Invalid rate value for currency %s: %s', $currency, $rate)
-                );
-            }
-        }
-    }
-
-    /**
-     * @throws ExchangeRateException
-     */
-    public function getRate(string $currency): float
-    {
-        if (!isset($this->rates[$currency])) {
-            throw ExchangeRateException::fromValidationError(
-                sprintf('Rate for currency %s not found', $currency)
-            );
-        }
-        return $this->rates[$currency];
     }
 }
